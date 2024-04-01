3b5d66d2ba841663f8f44343edc0d2143a4ee012
Edveika <edvinasbureika@gmail.com>
2024-04-01 22:23:51 +0300

    LED indicators, 0 distance fix for active flag

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,25 @@
+class LED {
+private:
+  int led_pin;
+
+public:
+  LED(const int pin) 
+  {
+    led_pin = pin;
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+  }
+
+  void turn_on() { digitalWrite(led_pin, HIGH); }
+  void turn_off() { digitalWrite(led_pin, LOW); }
+  void toggle(int ms_delay) 
+  {
+    turn_on();
+    delay(ms_delay);
+    turn_off();
+  }
+};
+
 class UltraSound {
 private:
   int trig_pin;  
@@ -32,12 +54,17 @@ public:
     float duration = pulseIn(echo_pin, HIGH);  
     return (duration * 0.0343) / 2; 
   }
+  float tmp;
   void run()
   {
     play_sound();
     float distance = get_distance();
-    if (distance <= max_distance)
-      active = true;
+    if (distance <= max_distance && distance != 0.0)
+    {
+      Serial.println(distance);
+      active = true;      
+    }
+
     else
       active = false;
   }
@@ -46,12 +73,19 @@ public:
 
 class Measure {
 private:
+  LED* leds[3];
   UltraSound* sensors[2];
   int i_start_sensor;
   unsigned long start_time;
   unsigned long end_time;
   bool measuring;
   float speed;
+  const int RED_LED = 11;
+  const int YELLOW_LED = 12;
+  const int GREEN_LED = 13;
+  const int I_LED_RED = 0;
+  const int I_LED_YELLOW = 1;
+  const int I_LED_GREEN = 2;
   
 public:
   Measure() {}
@@ -60,7 +94,10 @@ public:
     i_start_sensor = 1;
     measuring = false;
     sensors[0] = new UltraSound(8, 7, cm_max_distance);
-    sensors[1] = new UltraSound(10, 9, cm_max_distance);
+    sensors[1] = new UltraSound(11, 9, cm_max_distance);
+    leds[I_LED_RED] = new LED(RED_LED);
+    leds[I_LED_YELLOW] = new LED(YELLOW_LED);
+    leds[I_LED_GREEN] = new LED(GREEN_LED);
   }
   //~Measure() { delete[] sensors; }
 
@@ -80,10 +117,14 @@ public:
       sensors[i]->run();
 
     int i_cur_active_sensor = get_active_sensor();
+    
     if (!measuring)
     {
+      leds[I_LED_GREEN]->turn_on();
       if (i_cur_active_sensor == i_start_sensor)
       {
+        leds[I_LED_GREEN]->turn_off();
+        leds[I_LED_YELLOW]->turn_on();
         measuring = true;
         start_time = millis();
         Serial.print("Sensor: ");
@@ -94,6 +135,7 @@ public:
     {
       if (i_cur_active_sensor != -1 && i_cur_active_sensor != i_start_sensor)
       {
+        leds[I_LED_YELLOW]->turn_off();
         end_time = millis();
         measuring = false;
         unsigned long ellapsed = (end_time - start_time) / 1000;
@@ -102,27 +144,24 @@ public:
         float speed = 19 / ellapsed;
         Serial.print(speed);
         Serial.println(" cm/s");
+        leds[I_LED_RED]->turn_on();
+        delay(5000);
+        leds[I_LED_RED]->turn_off();
       }
     }
   }
 };
 
-float cm_max_distance = 30.0;
+float cm_max_distance = 10.0;
 Measure* measure;
 
 void setup() 
 {
-  Serial.begin(9600);
-  pinMode(11, OUTPUT);  
-  pinMode(12, OUTPUT); 
-  pinMode(13, OUTPUT); 
+  Serial.begin(9600); 
   measure = new Measure(cm_max_distance);
 }
 
 void loop() 
 {
-  digitalWrite(11, HIGH); 
-digitalWrite(12, HIGH); 
-digitalWrite(13, HIGH); 
   measure->run();
 }
